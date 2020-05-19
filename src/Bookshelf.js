6ecbc3a102a2aed35d4a8f95e6c553c9e25e4267
Jason Rosensweig <jayrosen1576@gmail.com>
2020-05-19 16:58:08 -0400

    added empty bookshelf message

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,14 +3,22 @@ import Book from './Book'
 
 function Bookshelf(props) {
   const { title, books, status, onSaveBook } = props
+  const booksList = books.filter(book => book.status === status)
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {books.filter(book => book.status === status).map(book => (
+          {booksList.map(book => (
             <li key={book.id}><Book book={book} onSaveBook={onSaveBook} /></li>
           ))}
+          {booksList.length === 0 && (
+            <div className='bookshelf-empty'>
+              <p>
+                You have no books in <span className='status-label'>{title}</span>
+              </p>
+            </div>
+          )}
         </ol>
       </div>
     </div>
